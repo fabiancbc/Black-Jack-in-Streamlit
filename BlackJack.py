8ef4e09dfcc39aba3d06cfb521aed9885d073035
fabiancbc <128000512+fabiancbc@users.noreply.github.com>
2023-04-08 17:45:31 -0400

    Instruction and code sanitation
    Instructions added and deleted unnecessary code

--- BlackJack.py
+++ BlackJack.py
@@ -8,9 +8,34 @@ import streamlit as st
 
 
 st.set_page_config(page_title="BlackJack Beta Version",page_icon=":game_die:", layout="centered")
-"st.session_state object:", st.session_state
+# "st.session_state object:", st.session_state
 
 st.title(":blue[Welcome to The BlackJack Simulator]")
+st.text("""This blackJack is inspired in Las Vegas BlackJack 
+As this program measures the probability of winning single hands, there in no 
+    option to double or split
+This is a single player simulator
+    Rules: 
+        1. If dealer's first 2 cards add up to 21 and user's first 2 cards add up
+            to 21, it's a push (tie).
+        2. If dealer's first 2 cards add up to 21 and user's first 2 cards don't
+            add up to 21, dealer automatically wins.
+        3. If user gets more than 21, it a bust and dealer automatically wins.
+        4. If user's cards add up to 21 or less then dealer has to hit until
+            reaches at least 17.
+        5. If dealer has 17 or more, must stand.
+    
+    User's Instructions:
+        1. Enter the number of decks you want to play with.
+        2. Enter number of simulations (the more simulation, the more accurate).
+        3. Enter position of cutting card in form of fractional percentage 
+            (Cutting card is where the dealer will stop and re-shuffle the 
+             decks so no counting cards happens in casinos)
+        4. Enter for each possible scenario where you want to stop the simulator
+            to hit. (Deffault values are provided and is the most recommended 
+            strategy regarding to 
+            https://www.cs.mcgill.ca/~rwest/wikispeedia/wpcd/wp/b/Blackjack.htm)
+        """)
 
 def shuffle_cards(sets):
     shoe = []
@@ -19,7 +44,6 @@ def shuffle_cards(sets):
             if i != 0:
                 shoe.append(i+1)
     return shoe
-
 
 
 
@@ -68,106 +92,10 @@ def deal_hand_player(player_name,lis):
     # print(f"{player_name} was dealt ({card1},{card2}): a value of {total}!")
     return [total, card1, card2,total1,total2]
 
-def deal_hand_dealer(player_name,lis):
-    funct = int_to_card(lis)
-    card1 = funct[0]
-    total1 = funct[1]
-    funct2 = int_to_card(lis)
-    card2 = funct2[0]
-    total2 = funct2[1]
-    total = total1 + total2
-    if total == 22:
-        total1 = 1
-        total = 12
-    # print(f"{player_name} was dealt ({card1})!")
-    return [total, card1, card2,total1,total2]
-
-
-def one_hand_blackjack():
-    dealer = deal_hand_dealer("Dealer",lis)
-    dealer_deck = [dealer[3],dealer[4]]
-    player = deal_hand_player("Player",lis)
-    player_deck = [player[3],player[4]]
-    player_total = player[0]
-    if dealer[0] == 21 and player_total != 21:
-        print("Dealer has BlackJack on hand, Dealer wins")
-        return None
-    elif dealer[0] == 21 and player_total == 21:
-        print("Its a tie!")
-        return None
-    elif dealer[0] != 21 and player_total == 21:
-        print("Player wins by BlackJack")
-        return None
-
-    hit1 = input("Press 1 if you want to hit, press 0 if you want to stop: ")
-    while hit1 == "1":
-        player_hit = int_to_card(lis)
-        player_deck.append(player_hit[1])
-        player_total += player_hit[1]
-        if player_total > 21:
-            for i in range(len(player_deck)):
-                if player_deck[i] == 11:
-                    player_total -= 10
-                    player_deck[i] = 1
-                if player_total < 22:
-                    break
-
-        print(f"Player was dealt ({player_hit[0]}): a new value of {player_total}!")
-        if player_total >= 21:
-            break
-        hit1 = input("Press 1 if you want to hit, press 0 if you want to stop: ")
-
-    print(f"Dealer has {dealer[1]}, {dealer[2]} for a total of {dealer[0]}")
-    partial = dealer[0]
-    while partial < 17 and player_total < 21:
-        functd = int_to_card(lis)
-        dealer_deck.append(functd[1])
-        partial += functd[1]
-        if partial > 21:
-
-            for i in range(len(dealer_deck)):
-                if dealer_deck[i] == 11:
-                    partial -= 10
-                    dealer_deck[i] = 1
-                if partial < 22:
-                    break
-        print(f"The dealer got a {functd[0]} for a new total of {partial}")
-    if partial > player_total:
-        if partial > 21:
-            print(f"Dealer lost as got higher than 21")
-        else:
-            print(f"Dealer wins with {partial} against {player_total}")
-    elif player_total > partial:
-        if player_total > 21:
-            print(f"Player lost as got higher than 21")
-        else:
-            print(f"Player wins with {player_total} against {partial}")
-    else:
-        print("Its a tie!!")
-
-
-
-
-
-
-# lis = [14,14,14,14,14,14,10,10,10,10,10,10,10,10]
-# # Individual Games
-# start = input("Wanna play? press 1: ")
-# while start =="1":
-#     shuffle1 = input("Press 1 if you want to shuffle the cards: ")
-#     if shuffle1 == "1":
-#          lis = shuffle_cards(sets = 8)
-#     elif lis == []:
-#         exit()
-#     one_hand_blackjack()
-#     print(lis)
-#     start = input("Wanna play? press 1: ")
-
-# Simulator
 
 
 def one_hand_blackjack_simulator():
-    dealer = deal_hand_dealer("Dealer",lis)
+    dealer = deal_hand_player("Dealer",lis)
     dealer_deck = [dealer[3],dealer[4]]
     player = deal_hand_player("Player",lis)
     player_deck = [player[3],player[4]]
@@ -275,10 +203,10 @@ def one_hand_blackjack_simulator():
     else:
         return "Push"
 
-
+st.write("---")
 decks = st.number_input("Enter number of decks: ", min_value = 1, step = 1, value = 6)
 number_of_games = st.number_input("Enter number of simulations: ", min_value = 1, step = 1, value = 100000)
-card_cut = st.number_input("Enter where in the shoe the cut card is(enter number between greater than 0 and less than 1): ", min_value = 0.01, max_value = 0.99, step = 0.01, value = 0.5)
+card_cut = st.number_input("Enter where in the shoe the cut card is positioned (enter number between greater than 0.2 and less than 1): ", min_value = 0.2, max_value = 0.99, step = 0.01, value = 0.5)
 # If dealer get the followings, hit until get which number
 if_2 = st.number_input("If dealer gets 2 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 13)
 if_3 = st.number_input("If dealer gets 3 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 13)
