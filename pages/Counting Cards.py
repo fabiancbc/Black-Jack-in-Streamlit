a0a8b5aeff02c4e8f3add974b0393fb5e0fa0626
fabiancbc <128000512+fabiancbc@users.noreply.github.com>
2023-04-10 11:56:44 -0400

    Counting Cards page
    Second feature of the webpage.

---
+++ pages/Counting Cards.py
@@ -0,0 +1,280 @@
+
+import streamlit as st
+import random 
+import pandas as pd
+count = 0
+
+
+
+def shuffle_cards(sets):
+    shoe = []
+    for i in range(14):
+        for j in range(sets*4):
+            if i != 0:
+                shoe.append(i+1)
+    return shoe
+
+
+
+def int_to_card(lis,hi_lo_count,grifin_ultimate_count):
+    # Replace each None with the correct values
+
+    card1 = random.choice(lis)
+    lis.remove(card1)
+
+    if card1 == 2:
+        hi_lo_count.append(1+hi_lo_count[-1])
+        griffin_ultimate_count.append(37+griffin_ultimate_count[-1])
+    elif card1 == 3:
+        hi_lo_count.append(1+hi_lo_count[-1])
+        griffin_ultimate_count.append(45+griffin_ultimate_count[-1])
+    elif card1 == 4:
+        hi_lo_count.append(1+hi_lo_count[-1])
+        griffin_ultimate_count.append(52+griffin_ultimate_count[-1])
+    elif card1 == 5:
+        hi_lo_count.append(1+hi_lo_count[-1])
+        griffin_ultimate_count.append(70+griffin_ultimate_count[-1])
+    elif card1 == 6:
+        hi_lo_count.append(1+hi_lo_count[-1])
+        griffin_ultimate_count.append(46+griffin_ultimate_count[-1])
+    elif card1 == 7:
+        hi_lo_count.append(0+hi_lo_count[-1])
+        griffin_ultimate_count.append(27+griffin_ultimate_count[-1])
+    elif card1 == 8:
+        hi_lo_count.append(0+hi_lo_count[-1])
+        griffin_ultimate_count.append(0+griffin_ultimate_count[-1])
+    elif card1 == 9:
+        hi_lo_count.append(0+hi_lo_count[-1])
+        griffin_ultimate_count.append(-17+griffin_ultimate_count[-1])
+        
+    if card1 == 10:
+        value1 = 10
+        card1 = "10"
+        hi_lo_count.append(-1+hi_lo_count[-1])
+        griffin_ultimate_count.append(-50+griffin_ultimate_count[-1])
+        
+    elif card1 == 11:
+        value1 = 10
+        card1 = "J"
+        hi_lo_count.append(-1+hi_lo_count[-1])
+        griffin_ultimate_count.append(-50+griffin_ultimate_count[-1])
+    elif card1 == 12:
+        value1 = 10
+        card1 = "Q"
+        hi_lo_count.append(-1+hi_lo_count[-1])
+        griffin_ultimate_count.append(-50+griffin_ultimate_count[-1])
+    elif card1 == 13:
+        value1 = 10
+        card1 = "K"
+        hi_lo_count.append(-1+hi_lo_count[-1])
+        griffin_ultimate_count.append(-50+griffin_ultimate_count[-1])
+    elif card1 == 14:
+        value1 = 11
+        card1 = "A"
+        hi_lo_count.append(-1+hi_lo_count[-1])
+        griffin_ultimate_count.append(-60+griffin_ultimate_count[-1])
+    else:
+        value1 = card1
+        card1 = f"{card1}"
+
+    return card1, value1
+
+
+
+
+def deal_hand_player(player_name,lis):
+
+    funct = int_to_card(lis,hi_lo_count,griffin_ultimate_count)
+    card1 = funct[0]
+    total1 = funct[1]
+    funct2 = int_to_card(lis,hi_lo_count,griffin_ultimate_count)
+    card2 = funct2[0]
+    total2 = funct2[1]
+    total = total1 + total2
+    if total == 22:
+        total1 = 1
+        total = 12
+
+    # print(f"{player_name} was dealt ({card1},{card2}): a value of {total}!")
+    return [total, card1, card2,total1,total2]
+
+
+
+def one_hand_blackjack_simulator():
+    dealer = deal_hand_player("Dealer",lis)
+    dealer_deck = [dealer[3],dealer[4]]
+    player = deal_hand_player("Player",lis)
+    player_deck = [player[3],player[4]]
+    player_total = player[0]
+    if dealer[0] == 21 and player_total != 21:
+
+        return "Dealer"
+    elif dealer[0] == 21 and player_total == 21:
+
+        return "Push"
+    elif dealer[0] != 21 and player_total == 21:
+
+        return "Player"
+
+    while player_total < 21:
+        if dealer[3] == 2:
+            if player_total < if_2:
+                pass
+            else:
+                break
+        elif dealer[3] == 3:
+            if player_total < if_3:
+                pass
+            else:
+                break
+        elif dealer[3] == 4:
+            if player_total < if_4:
+                pass
+            else:
+                break
+        elif dealer[3] == 5:
+            if player_total < if_5:
+                pass
+            else:
+                break
+        elif dealer[3] == 6:
+            if player_total < if_6:
+                pass
+            else:
+                break
+        elif dealer[3] == 7:
+            if player_total < if_7:
+                pass
+            else:
+                break
+        elif dealer[3] == 8:
+            if player_total < if_8:
+                pass
+            else:
+                break
+        elif dealer[3] == 9:
+            if player_total < if_9:
+                pass
+            else:
+                break
+        elif dealer[3] == 10:
+            if player_total < if_10:
+                pass
+            else:
+                break
+        elif dealer[1] == "A":
+            if player_total < if_A:
+                pass
+            else:
+                break
+
+        player_hit = int_to_card(lis,hi_lo_count,griffin_ultimate_count)
+        player_deck.append(player_hit[1])
+        player_total += player_hit[1]
+
+        if player_total > 21:
+            for i in range(len(player_deck)):
+                if player_deck[i] == 11:
+                    player_total -= 10
+                    player_deck[i] = 1
+                if player_total < 22:
+                    break
+
+
+
+    partial = dealer[0]
+    while partial < 17 and player_total < 21:
+        functd = int_to_card(lis,hi_lo_count,griffin_ultimate_count)
+        dealer_deck.append(functd[1])
+        partial += functd[1]
+        if partial > 21:
+
+            for i in range(len(dealer_deck)):
+                if dealer_deck[i] == 11:
+                    partial -= 10
+                    dealer_deck[i] = 1
+                if partial < 22:
+                    break
+
+    if partial > player_total:
+        if partial > 21:
+            return "Player"
+        else:
+            return "Dealer"
+    elif player_total > partial:
+        if player_total > 21:
+            return "Dealer"
+        else:
+            return "Player"
+    else:
+        return "Push"
+    
+    
+    
+decks = st.number_input("Enter number of decks: ", min_value = 1, step = 1, value = 6)
+number_of_games = st.number_input("Enter number of simulations: ", min_value = 1, step = 1, value = 100)
+card_cut = st.number_input("Enter where in the shoe the cut card is positioned (enter number between greater than 0.2 and less than 1): ", min_value = 0.2, max_value = 0.99, step = 0.01, value = 0.5)
+# If dealer get the followings, hit until get which number
+if_2 = st.number_input("If dealer gets 2 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 13)
+if_3 = st.number_input("If dealer gets 3 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 13)
+if_4 = st.number_input("If dealer gets 4 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 12)
+if_5 = st.number_input("If dealer gets 5 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 12)
+if_6 = st.number_input("If dealer gets 6 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 12)
+if_7 = st.number_input("If dealer gets 7 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 17)
+if_8 = st.number_input("If dealer gets 8 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 17)
+if_9 = st.number_input("If dealer gets 9 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 17)
+if_10 = st.number_input("If dealer gets 10 hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 17)
+if_A = st.number_input("If dealer gets A hit until you reach: ", min_value = 2, max_value = 21, step = 1, value = 17)
+count_player = 0
+count_dealer = 0
+count_push = 0
+
+
+
+
+wins = [0]
+losses = [0]
+ties = [0]
+
+if st.button("Click to start"):
+    hi_lo_count = [0]
+    griffin_ultimate_count = [0]
+    for i in range(number_of_games):
+        if i == 0:
+            lis = shuffle_cards(decks)
+        elif len(lis) <= decks*52*card_cut:
+            lis = shuffle_cards(decks)
+            griffin_ultimate_count.append(0)
+            hi_lo_count.append(0)
+        result = (one_hand_blackjack_simulator())
+        if result == "Player":
+            count_player += 1
+            wins.append(wins[-1]+1)
+            losses.append(losses[-1])
+            ties.append(ties[-1])
+        elif result == "Dealer":
+            count_dealer += 1
+            losses.append(losses[-1]+1)
+            wins.append(wins[-1])
+            ties.append(ties[-1])
+        else:
+            count_push += 1
+            ties.append(ties[-1]+1)
+            wins.append(wins[-1])
+            losses.append(losses[-1])
+        
+        
+        
+    dicc_keys = {"wins": wins,
+               "losses": losses,
+               "ties": ties}
+    dicc_keys2 = {"Hi-Lo Count System": hi_lo_count}
+    
+    dicc_keys3 = {"Griffin Ultimate System": griffin_ultimate_count}
+    dataframe = pd.DataFrame(dicc_keys)
+    counting_dataframe = pd.DataFrame(dicc_keys2)
+    counting_dataframe2 = pd.DataFrame(dicc_keys3)
+    st.write(f"Player won {count_player}, lost {count_dealer} and pushed {count_push} hands. Just won {count_player*100/number_of_games}% of the games")
+    st.line_chart(dataframe, width=0, height=0, use_container_width=True)
+    st.line_chart(counting_dataframe, width=0, height=0, use_container_width=True)
+    st.line_chart(counting_dataframe2, width=0, height=0, use_container_width=True)
